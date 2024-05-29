5f0b827e3f61ec2a8b9af4b766290ec0d9b06d96
vitzay996 <vitzay96@gmail.com>
2024-05-29 12:49:37 +0300

    First commit

---
+++ src/main/java/org/vitzay/Main.java
@@ -0,0 +1,19 @@
+package org.vitzay;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            BotTaskMaster botTaskMaster = new BotTaskMaster();
+            botsApi.registerBot(botTaskMaster);
+            System.out.println("Bot is running...");
+        } catch (TelegramApiException e) {
+            System.err.println("Failed to register bot: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
